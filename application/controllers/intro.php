eb452da0a48ef1b5631c29754991b3e814eb0afb
Marionette <belinecrecia@gmail.com>
2014-08-04 01:44:04 +0900

    Modify intro.php
    클래스명 고치느라 이름 바꿈

--- application/controllers/intro.php
+++ application/controllers/intro.php
@@ -1,14 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Intro extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
+	 * 		http://example.com/index.php/into
 	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
+	 * 		http://example.com/index.php/intro/index
 	 *	- or -
 	 * Since this controller is set as the default controller in 
 	 * config/routes.php, it's displayed at http://example.com/
@@ -23,5 +23,5 @@ class Welcome extends CI_Controller {
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file intro.php */
+/* Location: ./application/controllers/intro.php */
